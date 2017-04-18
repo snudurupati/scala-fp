988f3a6d11ce4851989e1c066d52081588a24182
snudurupati <raghav.sreeram@gmail.com>
2017-04-18 15:52:40 -0400

    Functions as objects.

---
+++ src/main/scala/first-class-function.scala
@@ -0,0 +1,16 @@
+/**
+  * Created by snudurupati on 4/18/2017.
+  *
+  * Functions are ordinary objects, so referred to as first-class functions
+  */
+
+val minus = new Function2[Int, Int, Int] {  //Function2 is a Scala trait representing a function with 2 arguments
+  def apply(a: Int, b: Int): Int = a - b
+}
+
+println(minus.apply(3, 5)) //same as below
+println(minus(3, 5))
+
+
+
+
